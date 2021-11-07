1027912e365e6035cd8f12d0a1b745c20e7edd44
CloudAppsDeveloper <developer.cloudapps@gmail.com>
2021-11-07 17:30:34 +0530

    package changes

--- src/main/java/com/cloudapps/developer/ApiGatewayApplication.java
+++ src/main/java/com/cloudapps/developer/ApiGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SimpleApiGateway1Application {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleApiGateway1Application.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
