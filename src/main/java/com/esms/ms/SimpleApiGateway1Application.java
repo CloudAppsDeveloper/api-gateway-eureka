4173c611b59c8f8d8dcebc114a5a55434032b698
CloudAppsDeveloper <developer.cloudapps@gmail.com>
2021-11-07 12:17:05 +0530

    initial commit

---
+++ src/main/java/com/esms/ms/SimpleApiGateway1Application.java
@@ -0,0 +1,15 @@
+package com.esms.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SimpleApiGateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiGateway1Application.class, args);
+	}
+
+}
